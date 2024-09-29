96f537ed608efe5897f7decb5c984526cba07c97
Fabian Döz <fabian@doez.info>
2024-09-29 13:14:56 +0200

    inital upload

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from "expo-status-bar";
+import {ScrollView, StyleSheet, Text, View} from "react-native";
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const name = "Julia";
+	return (
+		<ScrollView contentContainerStyle={styles.container}>
+			<View style={styles.container}>
+				<Text style={styles.headline}>myGardenJournal</Text>
+				<Text style={styles.text}>Hier entsteht {name}'s neue Garten Pflanzen und toDo App</Text>
+				<StatusBar style="auto"/>
+			</View>
+		</ScrollView>
+	);
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#333",
+		gap: 10,
+		alignItems: "center",
+		justifyContent: "flex-start",
+	},
+	headline: {
+		marginTop: 60,
+		color: "#fff",
+		fontSize: 24,
+	},
+	text: {
+		color: "green",
+	},
 });
